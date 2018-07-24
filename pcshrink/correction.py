fixed normalization

pcshrink/correction.py
@@ -140,20 +140,20 @@ class ShrinkageCorrector(object):
         """centers and scales the training gentoypes
         """
         # compute the emprical mean for each SNP ignoring NAs
-        self.mu = np.nanmean(self.Y_train, axis=0)
+        self.mu = np.nanmean(self.Y_train, axis=1)
         
         # compute the scaling factor using het or empircal sd
         if self.scale_method == "patterson":
-            f = np.nansum(self.Y_train, axis=0) / (2. * self.data.n)
+            f = np.nansum(self.Y_train, axis=1) / (2. * self.data.n)
             # TODO: check sqrt 
             self.sigma = np.sqrt(2 * f * (1. - f))
         elif self.scale_method == "empirical": 
-            self.sigma = np.nanstd(self.Y_train, axis=0)
+            self.sigma = np.nanstd(self.Y_train, axis=1)
         else:
             raise ValueError("scale method is not found please use patterson or empirical as arguments")
     
         # do the normalization and impute missing values to 0
-        self.Y_train = (self.Y_train - self.mu) / self.sigma
+        self.Y_train = np.transpose((self.Y_train.T - self.mu) / self.sigma)
         self.Y_train[np.isnan(Y_train)] = 0.0
 
     def train(self):

pcshrink/opt.py
@@ -8,7 +8,7 @@ import scipy.stats as stats
 
 class PCEM(object):
 
-    def __init__(self, y, Y, F_hat):
+    def __init__(self, Y, F_hat):
         """Expectation Maximazation (EM) for maximum likelihood estimation of 
         the probalistic formulation of principal components analysis (PCA). In the 
         zero noise limit this results in an efficent alternating least squares 
@@ -16,8 +16,6 @@ class PCEM(object):
 
         Arguments
         ---------
-        y : np.array
-            p x 1 normalized genotypes of the held-out individual 
         Y : np.array
             p x n normalized genotype matrix
         F : np.array
@@ -35,7 +33,6 @@ class PCEM(object):
             t x 1 array of likelihood values for each iteration
             of the EM
         """
-        self.y = y
         self.Y = Y
         self.p, self.n = Y.shape
 